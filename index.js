de4c1b5636aad9325f5749429c800fa758c91518
unknown <mdrafiqulakhter0@gmail.com>
2025-10-24 16:41:54 +0600

    changed  env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const app = express()
 
 const Stripe = require("stripe");
